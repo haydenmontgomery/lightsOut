46122c32b9d13945e92bb07d1b3730b78526d201
Hayden Montgomery <hayden.montgomery815@gmail.com>
2024-10-23 10:20:49 -0400

    Initial setup of lightsOut exercise

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Board from "./Board";
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <Board />
+      </div>
+    </>
+  );
+}
+
+export default App;
